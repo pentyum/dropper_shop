43e755a6217e2424b373b09de8af867f41df9d31
pentyum <pentyum@189.cn>
2018-11-10 22:07:33 +0800

    修复bug

--- src/main/java/com/piggest/minecraft/bukkit/depository/Update_component_listener.java
+++ src/main/java/com/piggest/minecraft/bukkit/depository/Update_component_listener.java
@@ -3,6 +3,7 @@ package com.piggest.minecraft.bukkit.depository;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
 
 public class Update_component_listener implements Listener {
@@ -17,8 +18,17 @@ public class Update_component_listener implements Listener {
 			if (level == 0) {
 				return;
 			}
-			if (!event.getInventory().contains(Update_component.component_item[level - 1])) {
+			CraftingInventory inventory = event.getInventory();
+			ItemStack basis = inventory.getContents()[5];
+			if(!Update_component.is_component(basis)) {
+				event.getWhoClicked().sendMessage("必须使用升级组件合成，而你使用的是"+basis.getType().name());
 				event.setCancelled(true);
+				return;
+			}
+			if(Update_component.get_level(basis)!=level-1) {
+				event.getWhoClicked().sendMessage("必须使用" + (level - 1) + "级升级组件合成" + level + "级升级组件");
+				event.setCancelled(true);
+				return;
 			}
 		}
 	}
