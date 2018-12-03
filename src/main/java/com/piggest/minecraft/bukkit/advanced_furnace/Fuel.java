0df07046df6b339f77ae546e6ed519db046db0be
pentyum <pentyum@189.cn>
2018-12-03 21:06:26 +0800

    更新燃料信息

--- src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
+++ src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
@@ -10,7 +10,7 @@ public enum Fuel {
 	blaze_rod(2.25, Status.solid, 2400), suger(1.1, Status.solid, 300), coal_powder(2.75, Status.solid, 1600),
 	lava_bucket(1.7, Status.liquid, 20000), gun_powder(12, Status.solid, 40), wool(1.2, Status.solid, 100),
 	carpet(1.2, Status.solid, 67), planks(1.4, Status.solid, 300), wheat_powder(9, Status.solid, 40),
-	stick(2.5, Status.solid, 100);
+	stick(2.15, Status.solid, 100);
 
 	private Status status;
 	private double power;
