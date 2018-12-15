c2acb12dff599ecd1b6c6f0eb076788172a70309
pentyum <pentyum@189.cn>
2018-12-15 14:24:21 +0800

    修复重构后的漏洞

--- src/main/java/com/piggest/minecraft/bukkit/dropper_shop/Dropper_shop_manager.java
+++ src/main/java/com/piggest/minecraft/bukkit/dropper_shop/Dropper_shop_manager.java
@@ -1,13 +1,12 @@
 package com.piggest.minecraft.bukkit.dropper_shop;
 
 import org.bukkit.Location;
+import org.bukkit.Material;
 
 import com.piggest.minecraft.bukkit.structure.Structure_manager;
 
 public class Dropper_shop_manager extends Structure_manager {
 	public static Dropper_shop_manager instance = null;
-	// private HashMap<Location, Dropper_shop> shops_map = new HashMap<Location,
-	// Dropper_shop>();
 
 	public Dropper_shop_manager() {
 		super(Dropper_shop.class);
@@ -16,13 +15,27 @@ public class Dropper_shop_manager extends Structure_manager {
 
 	@Override
 	public Dropper_shop find(String player_name, Location loc, boolean new_structure) {
-		Dropper_shop shop = this.get(loc);
-		if (player_name != null) {
-			if (!shop.get_owner_name().equalsIgnoreCase(player_name)) {
+		if (new_structure == false) {
+			Dropper_shop shop = this.get(loc);
+			if (shop == null) {
+				return null;
+			}
+			if (player_name != null) {
+				if (!shop.get_owner_name().equalsIgnoreCase(player_name)) {
+					return null;
+				}
+			}
+			return shop;
+		} else {
+			if (loc.getBlock().getType() == Material.DROPPER) {
+				Dropper_shop shop = new Dropper_shop();
+				shop.set_location(loc);
+				shop.set_owner(player_name);
+				return shop;
+			} else {
 				return null;
 			}
 		}
-		return shop;
 	}
 
 	@Override
