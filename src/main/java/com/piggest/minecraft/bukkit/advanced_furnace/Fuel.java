6dd4670d30d71a22f14f258ca3adf386f382a8cc
pentyum <pentyum@189.cn>
2019-07-15 00:24:57 +0800

    调整燃料判定机制

--- src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
+++ src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
@@ -8,8 +8,8 @@ import com.piggest.minecraft.bukkit.grinder.Powder;
 public enum Fuel {
 	coal(2, Status.solid, 1600), charcoal(1.8, Status.solid, 1600), coal_block(1.9, Status.solid, 16000),
 	blaze_rod(2.25, Status.solid, 2400), suger(1.1, Status.solid, 300), coal_powder(2.75, Status.solid, 1600),
-	lava_bucket(1.7, Status.liquid, 20000), gun_powder(12, Status.solid, 40), wool(1.2, Status.solid, 100),
-	carpet(1.2, Status.solid, 67), planks(1.4, Status.solid, 300), wheat_powder(9, Status.solid, 40),
+	lava_bucket(1.7, Status.liquid, 20000), gun_powder(12, Status.solid, 40), wool(1.1, Status.solid, 100),
+	carpet(1.1, Status.solid, 67), planks(1.4, Status.solid, 300), wheat_powder(9, Status.solid, 40),
 	stick(2.15, Status.solid, 100);
 
 	private Status status;
@@ -35,39 +35,43 @@ public enum Fuel {
 	}
 
 	public static Fuel get_fuel(ItemStack item) {
-		if (item.getType() == Material.COAL) {
-			return coal;
-		} else if (item.getType() == Material.CHARCOAL) {
-			return charcoal;
-		} else if (item.getType() == Material.COAL_BLOCK) {
-			return coal_block;
-		} else if (item.getType() == Material.BLAZE_ROD) {
-			return blaze_rod;
-		} else if (item.getType() == Material.LAVA_BUCKET) {
-			return lava_bucket;
-		} else if (item.getType() == Material.GUNPOWDER) {
-			return gun_powder;
-		} else if (item.getType() == Material.STICK) {
-			return stick;
-		} else if (item.getType() == Material.SUGAR) {
+		Material type = item.getType();
+		switch (type) {
+		case COAL:
+			return Fuel.coal;
+		case CHARCOAL:
+			return Fuel.charcoal;
+		case COAL_BLOCK:
+			return Fuel.coal_block;
+		case BLAZE_ROD:
+			return Fuel.blaze_rod;
+		case LAVA_BUCKET:
+			return Fuel.lava_bucket;
+		case GUNPOWDER:
+			return Fuel.gun_powder;
+		case STICK:
+			return Fuel.stick;
+		case SUGAR:
 			if (item.hasItemMeta()) {
 				if (Powder.is_powder(item, "coal_powder")) {
-					return coal_powder;
+					return Fuel.coal_powder;
 				} else if (Powder.is_powder(item, "wheat_powder")) {
-					return wheat_powder;
+					return Fuel.wheat_powder;
 				} else {
 					return null;
 				}
 			} else {
 				return suger;
 			}
-		} else if (item.getType().name().contains("WOOL")) {
-			return wool;
-		} else if (item.getType().name().contains("CARPET")) {
-			return carpet;
-		} else if (item.getType().name().contains("PLANKS")) {
-			return planks;
+		default:
+			if (type.name().contains("WOOL")) {
+				return Fuel.wool;
+			} else if (type.name().contains("CARPET")) {
+				return Fuel.carpet;
+			} else if (type.name().contains("PLANKS")) {
+				return Fuel.planks;
+			}
+			return null;
 		}
-		return null;
 	}
 }
