eedc934da4f12e17f2c99ebd137efa82fc3d74a5
pentyum <pentyum@189.cn>
2019-07-17 14:10:02 +0800

    木炭削弱

--- src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
+++ src/main/java/com/piggest/minecraft/bukkit/advanced_furnace/Fuel.java
@@ -6,11 +6,11 @@ import org.bukkit.inventory.ItemStack;
 import com.piggest.minecraft.bukkit.grinder.Powder;
 
 public enum Fuel {
-	coal(2, Status.solid, 1600), charcoal(1.8, Status.solid, 1600), coal_block(1.9, Status.solid, 16000),
+	coal(2, Status.solid, 1600), charcoal(1.45, Status.solid, 1600), coal_block(1.9, Status.solid, 16000),
 	blaze_rod(2.25, Status.solid, 2400), suger(1.1, Status.solid, 300), coal_powder(2.75, Status.solid, 1600),
 	lava_bucket(1.7, Status.liquid, 20000), gun_powder(12, Status.solid, 40), wool(1.1, Status.solid, 100),
 	carpet(1.1, Status.solid, 67), planks(1.4, Status.solid, 300), wheat_powder(9, Status.solid, 40),
-	stick(2.15, Status.solid, 100);
+	stick(2.1, Status.solid, 100);
 
 	private Status status;
 	private double power;
