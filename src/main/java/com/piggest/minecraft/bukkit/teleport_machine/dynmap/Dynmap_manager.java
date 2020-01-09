0151c26178caad04a041269ce00947182332a735
pentyum <pentyum@189.cn>
2020-01-09 14:14:39 +0800

    完善描述信息

--- src/main/java/com/piggest/minecraft/bukkit/teleport_machine/dynmap/Dynmap_manager.java
+++ src/main/java/com/piggest/minecraft/bukkit/teleport_machine/dynmap/Dynmap_manager.java
@@ -39,99 +39,46 @@ public class Dynmap_manager {
 		return set;
 	}
 
+	private String formatInfoWindow(Teleport_machine res) {
+		if (res == null)
+			return null;
+		if (res.getCustomName() == null)
+			return null;
+		String v = "<div class=\"regioninfo\"><div class=\"infowindow\"><span style=\"font-size:140%;font-weight:bold;\">%name%</span><br /> ";
+		v += "所有者: " + "<span style=\"font-weight:bold;\">%playerowners%</span><br />";
+		v += "天线长度: " + "<span style=\"font-weight:bold;\">%length%</span><br />";
+		v += "频率信息: " + "<br /><span style=\"font-weight:bold;\">";
+		v += "中心波长: <br />";
+		v += "带宽: ";
+		v += "</span>";
+		v += "</div></div>";
+
+		v = v.replace("%name%", res.getCustomName());
+		// v = v.replace("%playerowners%", res.getOwner());
+		return v;
+	}
 	/*
-	 * private String formatInfoWindow(Teleport_machine res) { if (res == null)
-	 * return null; if (res.getCustomName() == null) return null; String v =
-	 * "<div class=\"regioninfo\"><div class=\"infowindow\"><span style=\"font-size:140%;font-weight:bold;\">%regionname%</span><br /> "
-	 * + ChatColor.stripColor(plugin.msg(lm.General_Owner, "")) +
-	 * "<span style=\"font-weight:bold;\">%playerowners%</span><br />";
-	 * 
-	 * if (plugin.getConfigManager().DynMapShowFlags) v +=
-	 * ChatColor.stripColor(plugin.msg(lm.General_ResidenceFlags, "")) +
-	 * "<br /><span style=\"font-weight:bold;\">%flags%</span>"; v +=
-	 * "</div></div>";
-	 * 
-	 * if (plugin.getRentManager().isForRent(res.getCustomName())) v =
-	 * "<div class=\"regioninfo\"><div class=\"infowindow\">" +
-	 * ChatColor.stripColor(plugin.msg(lm.Rentable_Land, "")) +
-	 * "<span style=\"font-size:140%;font-weight:bold;\">%regionname%</span><br />"
-	 * + ChatColor.stripColor(plugin.msg(lm.General_Owner, "")) +
-	 * "<span style=\"font-weight:bold;\">%playerowners%</span><br />" +
-	 * ChatColor.stripColor(plugin.msg(lm.Residence_RentedBy, "")) +
-	 * "<span style=\"font-weight:bold;\">%renter%</span><br /> " +
-	 * ChatColor.stripColor(plugin.msg(lm.General_LandCost, "")) +
-	 * "<span style=\"font-weight:bold;\">%rent%</span><br /> " +
-	 * ChatColor.stripColor(plugin.msg(lm.Rent_Days, "")) +
-	 * "<span style=\"font-weight:bold;\">%rentdays%</span><br /> " +
-	 * ChatColor.stripColor(plugin.msg(lm.Rentable_AllowRenewing, "")) +
-	 * "<span style=\"font-weight:bold;\">%renew%</span><br /> " +
-	 * ChatColor.stripColor(plugin.msg(lm.Rent_Expire, "")) +
-	 * "<span style=\"font-weight:bold;\">%expire%</span></div></div>";
-	 * 
-	 * if (plugin.getTransactionManager().isForSale(res.getCustomName())) v =
-	 * "<div class=\"regioninfo\"><div class=\"infowindow\">" +
-	 * ChatColor.stripColor(plugin.msg(lm.Economy_LandForSale, " ")) +
-	 * "<span style=\"font-size:140%;font-weight:bold;\">%regionname%</span><br /> "
-	 * + ChatColor.stripColor(plugin.msg(lm.General_Owner, "")) +
-	 * "<span style=\"font-weight:bold;\">%playerowners%</span><br />" +
-	 * ChatColor.stripColor(plugin.msg(lm.Economy_SellAmount, "")) +
-	 * "<span style=\"font-weight:bold;\">%price%</span><br /></div></div>";
-	 * 
-	 * v = v.replace("%regionname%", resName); v = v.replace("%playerowners%",
-	 * res.getOwner()); String m = res.getEnterMessage(); v =
-	 * v.replace("%entermsg%", (m != null) ? m : ""); m = res.getLeaveMessage(); v =
-	 * v.replace("%leavemsg%", (m != null) ? m : ""); ResidencePermissions p =
-	 * res.getPermissions(); String flgs = "";
-	 * 
-	 * // remake Map<String, Boolean> all =
-	 * plugin.getPermissionManager().getAllFlags().getFlags(); String[] FLAGS = new
-	 * String[all.size()]; int ii = 0; for (Entry<String, Boolean> one :
-	 * all.entrySet()) { FLAGS[ii] = one.getKey(); ii++; }
-	 * 
-	 * for (int i = 0; i < FLAGS.length; i++) { if (p.isSet(FLAGS[i])) { if
-	 * (flgs.length() > 0) flgs += "<br/>"; boolean f = p.has(FLAGS[i], false); flgs
-	 * += FLAGS[i] + ": " + f; v = v.replace("%flag." + FLAGS[i] + "%",
-	 * Boolean.toString(f)); } else v = v.replace("%flag." + FLAGS[i] + "%", ""); }
-	 * v = v.replace("%flags%", flgs); RentManager rentmgr =
-	 * plugin.getRentManager(); TransactionManager transmgr =
-	 * plugin.getTransactionManager();
-	 * 
-	 * if (rentmgr.isForRent(res.getCustomName())) { boolean isrented =
-	 * rentmgr.isRented(resid); v = v.replace("%isrented%",
-	 * Boolean.toString(isrented)); String id = ""; if (isrented) id =
-	 * rentmgr.getRentingPlayer(resid); v = v.replace("%renter%", id);
-	 * 
-	 * v = v.replace("%rent%", rentmgr.getCostOfRent(resid) + ""); v =
-	 * v.replace("%rentdays%", rentmgr.getRentDays(resid) + ""); boolean renew =
-	 * rentmgr.getRentableRepeatable(resid); v = v.replace("%renew%", renew + "");
-	 * String expire = ""; if (isrented) { long time =
-	 * rentmgr.getRentedLand(resid).endTime; if (time != 0L) expire =
-	 * GetTime.getTime(time); } v = v.replace("%expire%", expire); }
-	 * 
-	 * if (transmgr.isForSale(res.getCustomName())) { boolean forsale =
-	 * transmgr.isForSale(resid); v = v.replace("%isforsale%",
-	 * Boolean.toString(transmgr.isForSale(resid))); String price = ""; if (forsale)
-	 * price = Integer.toString(transmgr.getSaleAmount(resid)); v =
-	 * v.replace("%price%", price); }
-	 * 
-	 * return v; }
-	 * 
-	 * private void addStyle(Teleport_machine res, Marker m) { AreaStyle as = new
-	 * AreaStyle(); int sc = 0xFF0000; int fc = 0xFF0000; try { sc =
-	 * Integer.parseInt(as.strokecolor.substring(1), 16); if
-	 * (plugin.getRentManager().isForRent(resid) &&
-	 * !plugin.getRentManager().isRented(resid)) fc =
-	 * Integer.parseInt(as.forrentstrokecolor.substring(1), 16); else if
-	 * (plugin.getRentManager().isForRent(resid) &&
-	 * plugin.getRentManager().isRented(resid)) fc =
-	 * Integer.parseInt(as.rentedstrokecolor.substring(1), 16); else if
-	 * (plugin.getTransactionManager().isForSale(resid)) fc =
-	 * Integer.parseInt(as.forsalestrokecolor.substring(1), 16); else fc =
-	 * Integer.parseInt(as.fillcolor.substring(1), 16); } catch
-	 * (NumberFormatException nfx) { } m.setLineStyle(as.strokeweight,
-	 * as.strokeopacity, sc); m.setFillStyle(as.fillopacity, fc); m.setRangeY(as.y,
-	 * as.y); }
-	 */
+	private void addStyle(Teleport_machine res, Marker m) {
+		AreaStyle as = new AreaStyle();
+		int sc = 0xFF0000;
+		int fc = 0xFF0000;
+		try {
+			sc = Integer.parseInt(as.strokecolor.substring(1), 16);
+			if (plugin.getRentManager().isForRent(resid) && !plugin.getRentManager().isRented(resid))
+				fc = Integer.parseInt(as.forrentstrokecolor.substring(1), 16);
+			else if (plugin.getRentManager().isForRent(resid) && plugin.getRentManager().isRented(resid))
+				fc = Integer.parseInt(as.rentedstrokecolor.substring(1), 16);
+			else if (plugin.getTransactionManager().isForSale(resid))
+				fc = Integer.parseInt(as.forsalestrokecolor.substring(1), 16);
+			else
+				fc = Integer.parseInt(as.fillcolor.substring(1), 16);
+		} catch (NumberFormatException nfx) {
+		}
+		m.setLineStyle(as.strokeweight, as.strokeopacity, sc);
+		m.setFillStyle(as.fillopacity, fc);
+		m.setRangeY(as.y, as.y);
+	}
+	*/
 	public void handle_teleport_machine_add(Teleport_machine res) {
 
 		if (res == null) {
@@ -144,7 +91,7 @@ public class Dynmap_manager {
 		double y = loc.getY();
 		double z = loc.getZ();
 
-		// String desc = formatInfoWindow(res);
+		String desc = formatInfoWindow(res);
 
 		Marker marker = null;
 		MarkerIcon icon = markerapi.getMarkerIcon("compass");
@@ -158,7 +105,7 @@ public class Dynmap_manager {
 			Dropper_shop_plugin.instance.getLogger().warning("Marker创建失败");
 			return;
 		}
-		// marker.setDescription(desc);
+		marker.setDescription(desc);
 		// addStyle(res, marker);
 		resareas.put(res.get_uuid(), marker);
 	}
