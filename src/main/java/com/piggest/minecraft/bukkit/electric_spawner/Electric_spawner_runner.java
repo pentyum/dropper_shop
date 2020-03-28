f607119598baa10358b60265fa3eef14847a2b51
pentyum <pentyum@189.cn>
2020-03-28 23:56:29 +0800

    添加刷怪机按时间扣钱机制

--- src/main/java/com/piggest/minecraft/bukkit/electric_spawner/Electric_spawner_runner.java
+++ src/main/java/com/piggest/minecraft/bukkit/electric_spawner/Electric_spawner_runner.java
@@ -9,6 +9,7 @@ import com.piggest.minecraft.bukkit.structure.Structure_runner;
 public class Electric_spawner_runner extends Structure_runner {
 	private Electric_spawner spawner;
 	private int completed_period = 0;
+	private int money_cost_period = 0;
 	private Electric_spawner_manager manager;
 
 	public Electric_spawner_runner(Electric_spawner spawner) {
@@ -30,26 +31,37 @@ public class Electric_spawner_runner extends Structure_runner {
 		if (local_difficulty < default_difficulty) {
 			return;
 		}
-		if (spawner.is_active()) {
-			EntityType spawn_entity_type = spawner.get_spawn_entity();
-			if (spawn_entity_type != null) {
-				Entity_spawn_config spawn_config = manager.spawn_config_map.get(spawn_entity_type);
-				int spawn_period = spawn_config.get_spawn_ticks() / this.get_cycle();
-				spawn_period = (int) (spawn_period * default_difficulty / local_difficulty);
-				if (this.completed_period >= spawn_period) {// 生成完成
-					this.completed_period = 0;
-					spawner.set_process(0);
-					Chunk chunk = spawner.get_location().getChunk();
-					long new_inhabited_time = chunk.getInhabitedTime() - spawn_config.get_inhabited_time_cost();
-					if (new_inhabited_time <= 0) {
-						new_inhabited_time = 0;
-					}
-					chunk.setInhabitedTime(new_inhabited_time);
-				} else {// 继续生成
-					this.completed_period++;
-					spawner.set_process(100 * this.completed_period / spawn_period);
+		if (!spawner.is_active()) {
+			return;
+		}
+		if (spawner.get_money() <= 0) {
+			return;
+		}
+		EntityType spawn_entity_type = spawner.get_spawn_entity();
+		if (spawn_entity_type != null) {
+			Entity_spawn_config spawn_config = manager.spawn_config_map.get(spawn_entity_type);
+			int spawn_period = spawn_config.get_spawn_ticks() / this.get_cycle();
+			spawn_period = (int) (spawn_period * default_difficulty / local_difficulty);
+			if (this.completed_period >= spawn_period) {// 生成完成
+				this.completed_period = 0;
+				spawner.set_process(0);
+				Chunk chunk = spawner.get_location().getChunk();
+				long new_inhabited_time = chunk.getInhabitedTime() - spawn_config.get_inhabited_time_cost();
+				if (new_inhabited_time <= 0) {
+					new_inhabited_time = 0;
 				}
+				chunk.setInhabitedTime(new_inhabited_time);
+			} else {// 继续生成
+				this.completed_period++;
+				spawner.set_process(100 * this.completed_period / spawn_period);
 			}
+		}
+
+		if (this.money_cost_period >= 4) {
+			this.money_cost_period = 0;
+			spawner.set_money(spawner.get_money() - 1);
+		} else {
+			this.money_cost_period++;
 		}
 	}
 
