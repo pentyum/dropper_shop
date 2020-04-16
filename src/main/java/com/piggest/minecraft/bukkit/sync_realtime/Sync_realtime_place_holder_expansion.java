ac8de9bc8020cb7f9dfaf315a550ab9c0b046952
pentyum <pentyum@189.cn>
2020-04-16 11:36:11 +0800

    添加expansion

---
+++ src/main/java/com/piggest/minecraft/bukkit/sync_realtime/Sync_realtime_place_holder_expansion.java
@@ -0,0 +1,65 @@
+package com.piggest.minecraft.bukkit.sync_realtime;
+
+import org.bukkit.entity.Player;
+
+import com.piggest.minecraft.bukkit.dropper_shop.Dropper_shop_plugin;
+
+import me.clip.placeholderapi.expansion.PlaceholderExpansion;
+
+public class Sync_realtime_place_holder_expansion extends PlaceholderExpansion {
+	private Dropper_shop_plugin plugin;
+
+	public Sync_realtime_place_holder_expansion(Dropper_shop_plugin plugin) {
+		this.plugin = plugin;
+	}
+
+	@Override
+	public boolean persist() {
+		return true;
+	}
+
+	@Override
+	public boolean canRegister() {
+		return true;
+	}
+
+	@Override
+	public String getAuthor() {
+		return plugin.getDescription().getAuthors().toString();
+	}
+
+	/**
+	 * The placeholder identifier should go here. <br>
+	 * This is what tells PlaceholderAPI to call our onRequest method to obtain a
+	 * value if a placeholder starts with our identifier. <br>
+	 * This must be unique and can not contain % or _
+	 *
+	 * @return The identifier in {@code %<identifier>_<value>%} as String.
+	 */
+	@Override
+	public String getIdentifier() {
+		return "droppershop";
+	}
+
+	@Override
+	public String getVersion() {
+		return plugin.getDescription().getVersion();
+	}
+
+	/**
+	 * This is the method called when a placeholder with our identifier is found and
+	 * needs a value. <br>
+	 * We specify the value identifier in this method. <br>
+	 * Since version 2.9.1 can you use OfflinePlayers in your requests.
+	 *
+	 * @param player     A {@link org.bukkit.Player Player}.
+	 * @param identifier A String containing the identifier/value.
+	 *
+	 * @return possibly-null String of the requested identifier.
+	 */
+	@Override
+	public String onPlaceholderRequest(Player player, String identifier) {
+		return null;
+	}
+
+}
