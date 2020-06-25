f10d15bdbb7baee27560594a458870498682f806
pentyum <pentyum@189.cn>
2020-06-26 00:43:16 +0800

    修复null

--- src/main/java/com/piggest/minecraft/bukkit/teleport_machine/Elements_container.java
+++ src/main/java/com/piggest/minecraft/bukkit/teleport_machine/Elements_container.java
@@ -2,6 +2,8 @@ package com.piggest.minecraft.bukkit.teleport_machine;
 
 import org.bukkit.inventory.Inventory;
 
+import javax.annotation.Nullable;
+
 public interface Elements_container {
 	public int get_amount(Element element);
 
@@ -9,18 +11,20 @@ public interface Elements_container {
 
 	public Inventory get_elements_gui();
 
-	public default void add(Elements_container other) {
-		for (Element element : Element.values()) {
-			this.set_amount(element, this.get_amount(element) + other.get_amount(element));
+	public default void add(@Nullable Elements_container other) {
+		if (other != null) {
+			for (Element element : Element.values()) {
+				this.set_amount(element, this.get_amount(element) + other.get_amount(element));
+			}
 		}
 	}
-	
+
 	public default void minus(Elements_container other) {
 		for (Element element : Element.values()) {
 			this.set_amount(element, this.get_amount(element) - other.get_amount(element));
 		}
 	}
-	
+
 	public default boolean has_enough(Elements_container other) {
 		for (Element element : Element.values()) {
 			if (this.get_amount(element) < other.get_amount(element)) {
