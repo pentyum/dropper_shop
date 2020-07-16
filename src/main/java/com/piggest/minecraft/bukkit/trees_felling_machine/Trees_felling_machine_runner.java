5d70968dccf1aa3923fd1c0f0a01092c59e01825
pentyum <pentyum@189.cn>
2020-07-17 00:24:41 +0800

    修复伐木机启动错误

--- src/main/java/com/piggest/minecraft/bukkit/trees_felling_machine/Trees_felling_machine_runner.java
+++ src/main/java/com/piggest/minecraft/bukkit/trees_felling_machine/Trees_felling_machine_runner.java
@@ -1,10 +1,17 @@
 package com.piggest.minecraft.bukkit.trees_felling_machine;
 
+import com.piggest.minecraft.bukkit.dropper_shop.Dropper_shop_plugin;
+import com.piggest.minecraft.bukkit.gui.Gui_slot_type;
 import com.piggest.minecraft.bukkit.structure.Structure;
+import org.bukkit.Material;
 import org.bukkit.block.Hopper;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import com.piggest.minecraft.bukkit.grinder.Grinder;
 import com.piggest.minecraft.bukkit.structure.Structure_runner;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
 
 public class Trees_felling_machine_runner extends Structure_runner {
     public Trees_felling_machine_runner(Trees_felling_machine_manager manager) {
@@ -16,6 +23,14 @@ public class Trees_felling_machine_runner extends Structure_runner {
         Trees_felling_machine machine = (Trees_felling_machine) structure;
         if (!machine.is_loaded()) {
             return;
+        }
+        int price = Dropper_shop_plugin.instance.get_price_config().get_start_trees_felling_machine_price();
+        Inventory gui = machine.getInventory();
+        ItemStack lever = gui.getItem(9);
+        if (!Grinder.is_empty(lever)) {
+            ItemMeta meta = lever.getItemMeta();
+            meta.setDisplayName("§r伐木机开关(开启需要" + Dropper_shop_plugin.instance.get_economy().format(price) + ")");
+            lever.setItemMeta(meta);
         }
         Hopper hopper = machine.get_axe_hopper();
         if (hopper != null) {
